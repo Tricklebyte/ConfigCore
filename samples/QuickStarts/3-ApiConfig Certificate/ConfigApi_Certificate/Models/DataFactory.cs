cbdae2c1630994255c85e5f6a06fe30c2bbd3675
frankjlinden <frankjlinden@gmail.com>
2020-01-11 06:40:51 -0500

    Quickstart 3 Api Client w/ Certificate Authentication COMPLETE

--- samples/QuickStarts/3-ApiConfig Certificate/ConfigApi_Certificate/Models/DataFactory.cs
+++ samples/QuickStarts/3-ApiConfig Certificate/ConfigApi_Certificate/Models/DataFactory.cs
@@ -11,11 +11,11 @@ namespace ConfigApi_Certificate.Models
         {
             return new List<ConfigEntity>()
         {
-            new ConfigEntity(){Id=1,AppId="ApiClient_EnvVarParam",SettingKey="Setting1",SettingValue="Setting 1 - Value for ApiClient_EnvVarParam"},
-            new ConfigEntity(){Id=2,AppId="ApiClient_EnvVarParam",SettingKey="Setting2",SettingValue="Setting 2 - Value for ApiClient_EnvVarParam"},
-            new ConfigEntity(){Id=3,AppId="ApiClient_EnvVarParam",SettingKey="Setting3",SettingValue="Setting 3 - Value for ApiClient_EnvVarParam"},
-            new ConfigEntity(){Id=4,AppId="ApiClient_EnvVarParam",SettingKey="Setting4",SettingValue="Setting 4 - Value for ApiClient_EnvVarParam"},
-            new ConfigEntity(){Id=5,AppId="ApiClient_EnvVarParam",SettingKey="Setting5",SettingValue="Setting 5 - Value for ApiClient_EnvVarParam"}
+            new ConfigEntity(){Id=1,AppId="ConfigApiClient_Certificate",SettingKey="Setting1",SettingValue="Setting 1 - Value for ConfigApiClient_Certificate"},
+            new ConfigEntity(){Id=2,AppId="ConfigApiClient_Certificate",SettingKey="Setting2",SettingValue="Setting 2 - Value for ConfigApiClient_Certificate"},
+            new ConfigEntity(){Id=3,AppId="ConfigApiClient_Certificate",SettingKey="Setting3",SettingValue="Setting 3 - Value for ConfigApiClient_Certificate"},
+            new ConfigEntity(){Id=4,AppId="ConfigApiClient_Certificate",SettingKey="Setting4",SettingValue="Setting 4 - Value for ConfigApiClient_Certificate"},
+            new ConfigEntity(){Id=5,AppId="ConfigApiClient_Certificate",SettingKey="Setting5",SettingValue="Setting 5 - Value for ConfigApiClient_Certificate"}
         };
 
         }
